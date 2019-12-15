9db264f959c30c5a0d61194a4f4c0d31c676fc0a
Hyunmin Park <hyunmin.park.career@gmail.com>
2019-12-15 21:52:35 +0000

    Automatically backed up by Learn

--- lib/hoa.rb
+++ lib/hoa.rb
@@ -8,6 +8,6 @@ BASE_HOA = {
 def add_character(show, name)
   # Write your implementation here
   # Should return the array of the 'show' argument
-  show[:chipmunks]<<"Dave"
+  show[:chipmunks] << "Dave"
   return show
 end
